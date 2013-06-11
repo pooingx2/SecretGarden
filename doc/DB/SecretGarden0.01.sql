62a8a2e643a642b7605ed529c0ee2056684e24ba
PJY <pooingx2@gmail.com>
2013-06-11 18:18:36 +0900

    DB & Client update

---
+++ doc/DB/SecretGarden0.01.sql
@@ -0,0 +1,57 @@
+/*
+create Database SecretGarden;
+GRANT ALL ON SecretGarden.* To 'pooingx2' IDENTIFIED BY 'SSM2013';
+*/
+
+CREATE TABLE User
+(
+	idUser			VARCHAR(20) NOT NULL,
+	pwd				VARCHAR(20) NOT NULL,
+	name			VARCHAR(20) NOT NULL,
+	email			VARCHAR(50) NOT NULL,
+	
+	PRIMARY KEY (idUser)
+);
+
+CREATE TABLE Root
+(
+	idRoot		INTEGER			NOT NULL AUTO_INCREMENT,
+	name		VARCHAR(20)		NOT NULL,
+	accessKey	VARCHAR(255)	NOT NULL,
+
+	PRIMARY KEY (idRoot)
+);
+
+CREATE TABLE File
+(
+	idFile		INTEGER			NOT NULL AUTO_INCREMENT,
+	name		VARCHAR(20)		NOT NULL,
+	path		VARCHAR(255)	NOT NULL,
+	metaPath	VARCHAR(255)	NOT NULL,
+
+	PRIMARY KEY (idFile)
+);
+
+CREATE TABLE User_Root
+(
+	idUser		VARCHAR(20) 	NOT NULL,
+	idRoot		INTEGER			NOT NULL,
+
+	FOREIGN KEY (idUser) REFERENCES User (idUser),
+	FOREIGN KEY (idRoot) REFERENCES Root (idRoot)
+);
+
+CREATE TABLE Root_File
+(
+	idRoot		INTEGER		 	NOT NULL,
+	idFile		INTEGER			NOT NULL,
+	
+	FOREIGN KEY (idRoot) REFERENCES Root (idRoot),
+	FOREIGN KEY (idFile) REFERENCES File (idFile)
+);
+
+/*
+ALTER TABLE User_Root
+ADD FOREIGN KEY (idFile) REFERENCES User (idFile);
+*/
+
